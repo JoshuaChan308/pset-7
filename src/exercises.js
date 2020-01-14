d98343fd228ad4be4a1a64fbc48f132677485685
Joshua Chan <JChan@ucvts.tec.nj.us>
2020-01-14 12:04:14 -0500

    yes

--- src/exercises.js
+++ src/exercises.js
@@ -56,7 +56,23 @@ function difference(numbers) {
 }
 
 function max(number) {
-  // write your code here
+  if (!number || number.length < 3 || number.some(isNaN) || number.length % 2 == 0) {
+    return undefined;
+  }
+  else{
+    var last = number[number.length - 1];
+    var middle = number[(number.length - 1)/2];
+    var first = number[0];
+    if(last >= middle && last >= first) {
+      return last;
+    }
+    else if(middle >= last && middle >= first) {
+      return middle;
+    }
+    else if(first >= last && first >= middle) {
+      return first;
+    }
+  }
 }
 
 function middle(values) {
